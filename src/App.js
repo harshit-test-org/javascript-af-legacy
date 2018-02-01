2e11a186140be726346dfb540714b8ec9e44e26b
Harshit Pant <pantharshit00@gmail.com>
2018-02-01 21:21:50 +0530

    feat(redux): Add redux with some startup code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import store from './redux/store'
 import Router from './routes/router'
 
 const App = () => (
-  <BrowserRouter>
-    <Router />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <Router />
+    </BrowserRouter>
+  </Provider>
 )
 
 export default App
