4103ac300edfcacb7d0db40aa061e599d292e8e6
Nicky Meuleman <nicky.meuleman@gmail.com>
2018-03-30 15:59:23 +0200

    feat: rework PostCard

--- components/PostCard.js
+++ components/PostCard.js
@@ -1,6 +1,7 @@
 import React, { Component, Fragment } from 'react'
 import styled from 'styled-components'
 import Router, { withRouter } from 'next/router'
+import GitHubIcon from '../assets/icons/github'
 
 const Card = styled.div`
   position: relative;
@@ -29,17 +30,61 @@ const CardContent = styled.div`
     font-weight: 400;
   }
 `
+const CardBottom = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-end;
+`
+const Author = styled.div`
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+  img {
+    border-radius: 50%;
+    height: 32px;
+    filter: grayscale(60%);
+  }
+  p {
+    margin-left: 8px;
+    padding: 0;
+    font-size: 14px;
+  }
+`
+
+const Actions = styled.div`
+  height: 24px;
+  width: 24px;
+`
+
 class RepoCard extends Component {
   handleCardClick = id => {
+    Router.push(`/repo/${id}`)
+  }
+  handleAuthorClick = (e, id) => {
+    e.stopPropagation()
     Router.push(`/user/${id}`)
   }
+  handleIconClick = (e, url) => {
+    e.stopPropagation()
+    Router.push(url)
+  }
   render () {
+    const { repoId, userId, title, text, author, image, url } = this.props
     return (
       <Fragment>
-        <Card onClick={ () => this.handleCardClick(this.props.userId) }>
+        <Card onClick={() => this.handleCardClick(repoId)}>
           <CardContent>
-            <h2>{ this.props.title }</h2>
-            <p>{ this.props.text }</p>
+            <h2>{title}</h2>
+            <p>{text}</p>
+            <CardBottom>
+              <Actions>
+                <GitHubIcon onClick={() => this.handleIconClick(url)} />
+              </Actions>
+              <Author onClick={e => this.handleAuthorClick(e, userId)}>
+                <img src={image} />
+                <p>{author}</p>
+              </Author>
+            </CardBottom>
           </CardContent>
         </Card>
       </Fragment>
