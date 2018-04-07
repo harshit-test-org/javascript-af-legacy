7c95d34a02999fb8a47a447684bffad9ba8b6d12
Harshit Pant <pantharshit00@gmail.com>
2018-04-07 23:29:32 +0530

    feat: Add new icons to the sidemenu

--- components/Sidemenu.js
+++ components/Sidemenu.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import Sidebar, { NavIcon } from './styles/Sidebar'
 import HomeIcon from '../assets/icons/home'
+import StarIcon from '../assets/icons/star'
 import SearchIcon from '../assets/icons/search'
-import ChatIcon from '../assets/icons/chat'
+import TrendingIcon from '../assets/icons/trending'
+// import ChatIcon from '../assets/icons/chat'
 import AccountIcon from '../assets/icons/account'
 import styled from 'styled-components'
 
@@ -29,11 +31,14 @@ class Sidemenu extends React.Component {
         <NavIcon href="/home" active={this.props.pathname === '/home'}>
           <HomeIcon />
         </NavIcon>
+        <NavIcon href="/social" active={this.props.pathname === '/social'}>
+          <TrendingIcon />
+        </NavIcon>
+        <NavIcon href="/search" active={this.props.pathname === '/search'}>
+          <StarIcon />
+        </NavIcon>
         <NavIcon href="/search" active={this.props.pathname === '/search'}>
           <SearchIcon />
-        </NavIcon>
-        <NavIcon href="/social" active={this.props.pathname === '/social'}>
-          <ChatIcon />
         </NavIcon>
         <NavIcon href="/profile" active={this.props.pathname === '/profile'}>
           <AccountIcon />
