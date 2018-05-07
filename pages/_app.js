49dd31bd4883cf9ddf2f7edb654fb92eebe51f4b
Harshit Pant <pantharshit00@gmail.com>
2018-05-07 20:15:07 +0530

    feat: Add sentry for error tracking

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import App, { Container } from 'next/app'
 import React from 'react'
 import { ErrorProvider, ErrorConsumer } from '../components/Error'
 import Snack, { Container as C } from '../components/SnackBar'
+import '../raven'
 
 export default class MyApp extends App {
   static async getInitialProps({ Component, router, ctx }) {
@@ -10,7 +11,6 @@ export default class MyApp extends App {
     if (Component.getInitialProps) {
       pageProps = await Component.getInitialProps(ctx)
     }
-
     return { pageProps }
   }
 
