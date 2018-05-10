8bb37cd49a1e69d4c8b1476de36422efd6992913
Nicky Meuleman <nicky.meuleman@gmail.com>
2018-05-10 14:44:27 +0200

    style(env): revert frontend env examples whitespace

--- frontend/.env.example.js
+++ frontend/.env.example.js
@@ -1,9 +1,9 @@
 module.exports = {
-    "BACKEND": "http://localhost:8080/api",
-    "WEBSOCKET": "ws://localhost:8080/api",
-    "ALGOLIA_APP_ID": "SEARCH_WILL_NOT_WORK_WITHOUT_IT",
-    "ALGOLIA_API_KEY": 'SEARCH_WILL_NOT_WORK_WITHOUT_IT',
-    "ALGOLIA_INDEX_NAME": "users",
-    "NODE_ENV": "development",
-    "SENTRY_DSN_CLIENT": "NO_NEED_TO_ADD_IN_DEV"
-  }
+  BACKEND: "http://localhost:8080/api",
+  WEBSOCKET: "ws://localhost:8080/api",
+  ALGOLIA_APP_ID: "SEARCH_WILL_NOT_WORK_WITHOUT_IT",
+  ALGOLIA_API_KEY: "SEARCH_WILL_NOT_WORK_WITHOUT_IT",
+  ALGOLIA_INDEX_NAME: "users",
+  NODE_ENV: "development",
+  SENTRY_DSN_CLIENT: "NO_NEED_TO_ADD_IN_DEV"
+};
