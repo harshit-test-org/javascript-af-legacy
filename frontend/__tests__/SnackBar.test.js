ecd7f52316e1a05ca43015dd9ca8c5fb458104a0
Harshit Pant <pantharshit00@gmail.com>
2018-05-12 21:51:10 +0530

    test(Snackbar.test.js): Add without action test

--- frontend/__tests__/SnackBar.test.js
+++ frontend/__tests__/SnackBar.test.js
@@ -4,7 +4,8 @@ import SnackBar from '../components/SnackBar'
 
 describe('SnackBar', () => {
   const actionClick = jest.fn()
-  const fullComp = <SnackBar show message="test message" actionText="action" actionClick={actionClick} />
+  const fullComp = <SnackBar message="test message" actionText="action" actionClick={actionClick} />
+  const noActionComp = <SnackBar message="test message" />
   test('should render with text and action (Snapshot)', () => {
     const wrapper = render(fullComp)
     expect(wrapper.container.firstChild).toMatchSnapshot()
@@ -12,5 +13,9 @@ describe('SnackBar', () => {
   test('should call onClick when action is clicked', () => {
     Simulate.click(render(fullComp).getByText('action'))
     expect(actionClick).toBeCalled()
+  })
+  test('should render without action', () => {
+    const { container } = render(noActionComp)
+    expect(container.firstChild.querySelector('a')).toBeNull()
   })
 })
