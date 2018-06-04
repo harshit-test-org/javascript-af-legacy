12a91cee80b280ec73d4ee56fdc5d522a8645ccf
Nicky Meuleman <nicky.meuleman@gmail.com>
2018-06-04 22:29:57 +0200

    test: move cypress to root monorepo

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -16,5 +16,6 @@
 // Import commands.js using ES2015 syntax:
 import './commands'
 import 'cypress-testing-library/add-commands'
+
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
